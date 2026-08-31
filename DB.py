76a9ac99e2721a7ae8f7f8a912cf4bc55276735f
Rekushi <170186554+RekuNote@users.noreply.github.com>
2026-08-31 20:28:18 +0100

    updated for py3 port

--- DB.py
+++ DB.py
@@ -1,11 +1,10 @@
-DB_type = "plaintext"#"plaintext" or "mondoDB"
-
+DB_type = "plaintext"  # "plaintext" or future backends
 
 if DB_type == "plaintext":
-	from database import Database
-elif DB_type == "mondoDB":#not yet implemented
-	from database import Database#hue
+    from database import Database
+elif DB_type == "mondoDB":  # legacy placeholder; not implemented
+    from database import Database
 else:
-	import sys
-	print "Unsupported database type \"%s\"" % DB_type
-	sys.exit()
+    import sys
+    print('Unsupported database type "%s"' % DB_type)
+    sys.exit(1)
